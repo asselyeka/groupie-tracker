9863700619c06a1b888846050a571e3bf5a26d64
assel <a.moldasheva@gmail.com>
2020-02-22 18:40:31 +0600

    fix: add style

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
-var art Artists
+var art []Artists
 
-type Artists []struct {
-	Id                int      `json:id`
-	Image             string   `json:image`
-	Name              string   `json:name`
-	Members           []string `json:members`
-	CreationDate_year int      `json:creationDate`
-	FirstAlbum_date   int      `json:firstAlbum`
-	Locations         string   `json:locations`
-	ConcertDates      string   `json:concertDates`
-	Relations         string   `json:relations`
+type Artists struct {
+	Id           int      `json:id`
+	Image        string   `json:image`
+	Name         string   `json:name`
+	Members      []string `json:members`
+	CreationDate int      `json:creationDate`
+	FirstAlbum   string   `json:firstAlbum`
+	Locations    string   `json:locations`
+	ConcertDates string   `json:concertDates`
+	Relations    string   `json:relations`
 }
 
 func ParseArtists(url string) {
@@ -31,9 +33,9 @@ func ParseArtists(url string) {
 
 	defer response.Body.Close()
 
-	ourStr, orr := ioutil.ReadAll(response.Body)
-	if orr != nil {
-		panic(orr)
+	ourStr, err1 := ioutil.ReadAll(response.Body)
+	if err1 != nil {
+		panic(err1)
 	}
 
 	json.Unmarshal(ourStr, &art)
@@ -41,12 +43,73 @@ func ParseArtists(url string) {
 	fmt.Println("Artists")
 }
 
+// locations := "https://groupietrackers.herokuapp.com/api/locations"
+// dates := "https://groupietrackers.herokuapp.com/api/dates"
+// relation :=	"https://groupietrackers.herokuapp.com/api/relation" (
+
+var artists = "https://groupietrackers.herokuapp.com/api/artists"
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+
+	body := r.FormValue("body")
+
+	//font := r.FormValue("fonts")
+
+	data := SearchArtist(body)
+
+	for i := range data {
+		fmt.Println(data[i].CreationDate)
+	}
+
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+}
+
+func SearchArtist(name string) []Artists {
+	if name == "" {
+		return nil
+	}
+	var search_artist []Artists
+
+	for i, artist := range art {
+		if name[0] == artist.Name[0] {
+			lenght_name := 0
+			for _, l := range []byte(name) {
+				if l == art[i].Name[lenght_name] {
+					lenght_name++
+				} else {
+					break
+				}
+			}
+			if len(name) == lenght_name {
+				search_artist = append(search_artist, art[i])
+			}
+		}
+	}
+	return search_artist
+}
+
 func main() {
-	artists := "https://groupietrackers.herokuapp.com/api/artists"
-	// locations := "https://groupietrackers.herokuapp.com/api/locations"
-	// dates := "https://groupietrackers.herokuapp.com/api/dates"
-	// relation :=	"https://groupietrackers.herokuapp.com/api/relation"
+
 	ParseArtists(artists)
 	println(art[0].Name)
+
+	http.HandleFunc("/", mainPage)
+
+	port := ":8080"
+	println("Server listen on port:", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("Listen and Server", err)
+	}
 
 }
